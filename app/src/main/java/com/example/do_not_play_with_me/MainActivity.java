64b89188c8df752b4646ad9aea6538fffc92961a
Nursena  Karakulah <senafrakara@gmail.com>
2021-05-22 15:29:25 +0300

    initializing

---
+++ app/src/main/java/com/example/do_not_play_with_me/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.do_not_play_with_me;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
